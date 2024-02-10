6e4430338ac59532e961927992e6498f3554d4aa
YohanBROUTHIER <yohan.brouthier@oclock.school>
2024-02-10 07:35:03 +0100

    update error handling

--- src/store/api.js
+++ src/store/api.js
@@ -1,110 +1,82 @@
 import { apiBaseUrl } from "../config.js";
+import AppError from "../helpers/appError.js";
 
 class CoreApi {
   static routeName;
-  static  _errorHandler(error) {
-    console.error(error);
-    return null;
-  }
-  static async create(data) {
-    async function fetchData(token) {
-      const httpResponse = await fetch(`${apiBaseUrl}/${this.routeName}`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
-        body: JSON.stringify(data)
-      });
-  
-      if (! httpResponse.ok) {
-        throw new Error("Invalid responce");
-      }
-  
-      return await httpResponse.json();
-    }
 
-    return await this.getValidToken()
-      .then(fetchData)
-      .catch(this._errorHandler)
+  static async create(data) {
+    const token = await this.getValidToken();
+
+    const httpResponse = await fetch(`${apiBaseUrl}/${this.routeName}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
+      body: JSON.stringify(data)
+    });
+
+    this.errorHandler(httpResponse);
+
+    return await httpResponse.json();
+
   }
   static async get(id) {
-    async function fetchData() {
-      const httpResponse = await fetch(`${apiBaseUrl}/${this.routeName}/${id}`);
-  
-      if (!httpResponse.ok) {
-        throw new Error("Invalid responce");
-      }
-  
-      return await httpResponse.json();
-    }
+    const httpResponse = await fetch(`${apiBaseUrl}/${this.routeName}/${id}`);
 
-    return await fetchData().catch(this._errorHandler)
+    this.errorHandler(httpResponse);
+
+    return await httpResponse.json();
   }
   static async getAll() {
-    async function fetchData() {
-      const httpResponse = await fetch(`${apiBaseUrl}/${this.routeName}`);
-  
-      if (!httpResponse.ok) {
-        throw new Error("Invalid responce");
-      }
-  
-      return await httpResponse.json();
-    }
+    const httpResponse = await fetch(`${apiBaseUrl}/${this.routeName}`);
 
-    return await fetchData().catch(this._errorHandler)
+    this.errorHandler(httpResponse);
+
+    return await httpResponse.json();
   }
   static async update(id, data) {
-    async function fetchData(token) {
-      const httpResponse = await fetch(`${apiBaseUrl}/${this.routeName}/${id}`, {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
-        body: JSON.stringify(data)
-      });
+    const token = await this.getValidToken();
+
+    const httpResponse = await fetch(`${apiBaseUrl}/${this.routeName}/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
+      body: JSON.stringify(data)
+    });
+  
+    this.errorHandler(httpResponse);
     
-      if (!httpResponse.ok) {
-        throw new Error("Invalid responce");
-      }
-      
-      return await httpResponse.json();
-    }
-    return await this.getValidToken()
-      .then(fetchData)
-      .catch(this._errorHandler);
+    return await httpResponse.json();
   }
   static async delete(id) {
-    async function fetchData(token) {
-      const httpResponse = await fetch(`${apiBaseUrl}/${this.routeName}/${id}`, {
-        method: "DELETE",
-        headers: { Authorization: `Bearer ${token}` }
-      });
-    
-      if (!httpResponse.ok) {
-        console.error(httpResponse.status);
-        return false;
-      }
-    
-      return true;
-    }
-    return await this.getValidToken()
-      .then(fetchData)
-      .catch(this._errorHandler);
+    const token = await this.getValidToken();
+
+    const httpResponse = await fetch(`${apiBaseUrl}/${this.routeName}/${id}`, {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${token}` }
+    });
+  
+    this.errorHandler(httpResponse);
+  
+    return true;
   }
   static async getValidToken() {
-    async function fetchData() {
-      const httpResponse = await fetch(`${apiBaseUrl}/signin`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: sessionStorage.setItem("getItem")
-      });
-      
-      if (! httpResponse.ok) {
-        throw new Error("Invalid responce");
-      }
-  
-      const response = await httpResponse.json();
-      sessionStorage.setItem("token", JSON.stringify(response.token));
-      return response.token;
-    }
+    const httpResponse = await fetch(`${apiBaseUrl}/signin`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: sessionStorage.setItem("getItem")
+    });
     
-    return new Promise(fetchData)
+    this.errorHandler(httpResponse);
+
+    const response = await httpResponse.json();
+    sessionStorage.setItem("token", JSON.stringify(response.token));
+    return response.token;
+  }
+  static async errorHandler(res) {
+    if (httpResponse.ok) return;
+    if (!res.bodyUsed) {
+      throw new AppError(res.statusText, {httpStatu: res.status});
+    }
+    const {error} = await res.json()
+    throw new AppError(error, {httpStatu: res.status});
   }
 }
 
@@ -124,42 +96,30 @@ export class UserApi extends CoreApi {
   static routeName = "user";
 
   static async create(data) {
-    async function fetchData() {
-      const httpResponse = await fetch(`${apiBaseUrl}/signup`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-      });
-  
-      if (! httpResponse.ok) {
-        throw new Error("Invalid responce");
-      }
-  
-      return true;
-    }
+    const httpResponse = await fetch(`${apiBaseUrl}/signup`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
 
-    return await fetchData().catch(this._errorHandler)
+    this.errorHandler(httpResponse);
+
+    return true;
   }
   static async signin(data) {
-    async function fetchData() {
-      const httpResponse = await fetch(`${apiBaseUrl}/signin`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-      });
-  
-      if (! httpResponse.ok) {
-        throw new Error("Invalid responce");
-      }
-      
-      const response = await httpResponse.json();
-      sessionStorage.setItem("token", JSON.stringify(response.token));
-      sessionStorage.setItem("userDataConnection", JSON.stringify(data));
+    const httpResponse = await fetch(`${apiBaseUrl}/signin`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
 
-      return response.user;
-    }
+    this.errorHandler(httpResponse);
+    
+    const response = await httpResponse.json();
+    sessionStorage.setItem("token", JSON.stringify(response.token));
+    sessionStorage.setItem("userDataConnection", JSON.stringify(data));
 
-    return await fetchData().catch(this._errorHandler)
+    return response.user;
   }
   static signout() {
     sessionStorage.removeItem("token");
@@ -167,51 +127,36 @@ export class UserApi extends CoreApi {
   }
 
   static async RequestResetPasword(data) {
-    async function fetchData() {
-      const httpResponse = await fetch(`${apiBaseUrl}/reset-password/`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-      });
-      
-      if (!httpResponse.ok) {
-        throw new Error("Invalid responce");
-      }
-      
-      return true;
-    }
-    return await fetchData().catch(this._errorHandler)
+    const httpResponse = await fetch(`${apiBaseUrl}/reset-password/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+    
+    this.errorHandler(httpResponse);
+    
+    return true;
   }
   static async validateAccount(uuid) {
-    async function fetchData() {
-      const httpResponse = await fetch(`${apiBaseUrl}/user/account/${uuid}`, {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" }
-      });
-      
-      if (!httpResponse.ok) {
-        throw new Error("Invalid responce");
-      }
-      
-      return true;
-    }
-    return await fetchData().catch(this._errorHandler)
+    const httpResponse = await fetch(`${apiBaseUrl}/user/account/${uuid}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" }
+    });
+    
+    this.errorHandler(httpResponse);
+    
+    return true;
   }
   static async validatePassword(uuid, data) {
-    async function fetchData() {
-      const httpResponse = await fetch(`${apiBaseUrl}/user/password/${uuid}`, {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-      });
-      
-      if (!httpResponse.ok) {
-        throw new Error("Invalid responce");
-      }
-      
-      return true;
-    }
-    return await fetchData().catch(this._errorHandler)
+    const httpResponse = await fetch(`${apiBaseUrl}/user/password/${uuid}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+    
+    this.errorHandler(httpResponse);
+    
+    return true;
   }
  
 }
