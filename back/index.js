54b725b02dc6c92f262ce14781bd5b6c0b179f8e
Ben Chéri-Zécoté <benjamin.cheri-zecote@oclock.school>
2025-10-22 16:35:09 +0200

    new PAAS deployment

--- back/index.js
+++ back/index.js
@@ -7,7 +7,8 @@ import app from './src/index.js'; //Module express
 const httpServer = createServer(app);
 
 const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL;
 
 httpServer.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Server listening on ${BASE_URL ? BASE_URL : `http://localhost:${PORT}`}`);
 });
--- front/src/config.js
+++ front/src/config.js
@@ -1,1 +1,1 @@
-export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'https://thinkeat.fly.dev/api';
+export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'https://thinkeat-production.up.railway.app/api';
