4bf4058ae833d1140614502d853fe5e2cf775244
Ben Chéri-Zécoté <benjamin.cheri-zecote@oclock.school>
2026-06-22 15:36:44 +0200

    chore: stop tracking .DS_Store files and add root .gitignore, update link portfolio

--- front/src/components/Layout/Partials/Footer/index.jsx
+++ front/src/components/Layout/Partials/Footer/index.jsx
@@ -5,13 +5,14 @@ import LogoHat from "../../UXElements/icons/LogoHat";
 
 const Footer = () => {
     const {isAside} = useSelector((state) => state.isAside);
+    const currentYear = new Date().getFullYear()
 
     return(
         <footer id="footer" className={style.footer} style={isAside? {gridColumn: '2 / -1'}:{gridColumn: '1 / -1'} }>
             <div>
-                <p>©ThinkEat 2025</p>
+                <p>©ThinkEat {currentYear}</p>
                 <NavLink>Gestion des cookies</NavLink>
-                <a href='https://www.chezben.io/' target="_blank" rel="noreferrer">chezben.io</a>
+                <a href='https://www.chezben.fr/' target="_blank" rel="noreferrer">chezben.fr</a>
             </div>
             <LogoHat size={2.8}/>
         </footer>
